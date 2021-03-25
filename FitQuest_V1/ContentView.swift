14f163111f35528bb7f6058ca5c3013129d9d0ba
Sophie <sophiemcq@live.com>
2021-03-25 14:08:05 -0700

    Initial Commit

---
+++ FitQuest_V1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitQuest_V1
+//
+//  Created by Sophie McQuain on 3/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitQuest_V1/FitQuest_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuest_V1App.swift
+//  FitQuest_V1
+//
+//  Created by Sophie McQuain on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FitQuest_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
